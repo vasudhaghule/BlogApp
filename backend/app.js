3d568ab3270f4a83794411b648bb0096be0ef776
Vasudha Ghule <146065753+Vasudhaghule123@users.noreply.github.com>
2024-10-27 13:31:51 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import  mongoose from 'mongoose'
+import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
+import cors from "cors"
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user",router);  //http://localhost:5000/api/user/login
+app.use("/api/blog", blogRouter);
+
+mongoose.connect('mongodb+srv://BlogApp:Gayatri@cluster0.txzfuc6.mongodb.net/Blog?retryWrites=true&w=majority')
+.then(()=>app.listen(5000))
+.then(()=>
+    console.log("Connected To Database and Listening To Localhost 5000"))
+.catch((err) => console.log(err));
+
+
+// password : Gayatri
+// username : BlogApp
